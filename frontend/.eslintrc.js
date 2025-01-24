e73d46f1d88d1b15ec1cae9b04600f4b658a7044
art-42 <artursv2001@gmail.com>
2025-01-24 16:15:29 -0300

    fiz: adicionado caminho no lint para não dar erros de import

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,4 +2,11 @@
 module.exports = {
   extends: 'expo',
   ignorePatterns: ['/dist/*'],
+  settings: {
+    'import/resolver': {
+      alias: [
+        ['@', './src'],  // adjust the path if your `src` directory is located elsewhere
+      ],
+    },
+  },
 };
